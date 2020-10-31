8cf8bd6db3edd746cd76ee08832346f32302b316
nemaye <yameen.abdullah.khan@gmail.com>
2020-11-01 00:49:03 +0530

    updated

--- app/src/main/java/com/example/bookit/DownloadActivity.java
+++ app/src/main/java/com/example/bookit/DownloadActivity.java
@@ -2,14 +2,19 @@ package com.example.bookit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
-import java.util.Objects;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
 
 public class DownloadActivity extends AppCompatActivity {
 
+    static String link;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +22,24 @@ public class DownloadActivity extends AppCompatActivity {
 
         String link = getIntent().getStringExtra("hyperL");
         Log.d("link",link);
+
+        new Dactivity().execute();
+    }
+
+    public class Dactivity extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids){
+            try {
+                Document html = Jsoup.connect(link).get();
+                Element content = html.getElementsByTag("tr").first().getElementsByAttribute("href").first();
+                String s = content.attr("href");
+                Log.d("linkIs",s);
+                Log.d("haha","hahaha");
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
